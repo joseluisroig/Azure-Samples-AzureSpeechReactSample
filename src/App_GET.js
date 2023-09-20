00b0a15169ae98cb4de6d2207f03dbe5e806848c
joseluisroig <jroig@uchceu.es>
2023-09-20 15:38:03 +0200

    app_get

--- src/App_GET.js
+++ src/App_GET.js
@@ -1,8 +1,8 @@
-import React, { useState, useEffect, useRef } from "react";
+import React, { useState, useEffect, useRef, useCallback } from "react";
 import * as speechsdk from "microsoft-cognitiveservices-speech-sdk";
 //import TextToSpeech from './TextToSpeech.js';
 //import FileUpload from './FileUpload';
-//import Header from "./Header";
+
 
 import "./App.css";
 
@@ -16,12 +16,10 @@ const App = () => {
   //const peerConnectionRef = useRef(null);
   const peerConnectionRef = useRef([
     { id: 0, connection: null, active: false },
-    { id: 1, connection: null, active: false },
-    { id: 2, connection: null, active: false },
-    { id: 3, connection: null, active: false },
+
   ]);
   const userRefIndexConnection = useRef(0);
-  let indexConnection = userRefIndexConnection.current;
+  const indexConnection = 0;
 
   const streamIdRef = useRef(null);
   const sessionIdRef = useRef(null);
@@ -30,132 +28,33 @@ const App = () => {
   const SPEECH_KEY = '0c029cad0e45489fa76bca71569b0f3e';
   const SPEECH_REGION = 'westeurope';
   const IMG_URL_ROIG = 'https://create-images-results.d-id.com/auth0%7C646f6fd64196da85cb62a776/upl_dtCm20p57vc6Gz-kKC8oW/image.jpeg';
-  const avatarImgUrl = useRef(IMG_URL_ROIG);
-  const azureVoiceId = useRef('es-ES-DarioNeural');
-  const avatar = useRef('roig');
-  const speechRecognitionLanguage = useRef('es-ES');
-  //variable modo video que solo puede ser VIDDEO o AUDIO
- // const modoVideoAudioChat = useRef('VIDEO');
-
- const urlParams = new URLSearchParams(window.location.search);
- const avatarType = urlParams.get('avatar');
- 
-
-  const getTokenOrRefresh = async () => {
-    const response = await fetch(`https://${SPEECH_REGION}.api.cognitive.microsoft.com/sts/v1.0/issueToken`, {
-      method: "POST",
-      headers: {
-        "Ocp-Apim-Subscription-Key": SPEECH_KEY,
-      },
-    });
-
-    const authToken = await response.text();
-    return { authToken, region: SPEECH_REGION };
-  };
-
-  useEffect(() => {
-    const initialize = async () => {
-      const tokenRes = await getTokenOrRefresh();
-      if (tokenRes.authToken === null) {
-        setDisplayText(`FATAL_ERROR: ${tokenRes.error}`);
-      }
-    };
-    initialize();
-
-  }, []);
-
-  const sttFromMic = async () => {
-    const tokenObj = await getTokenOrRefresh();
-    const speechConfig = speechsdk.SpeechConfig.fromAuthorizationToken(tokenObj.authToken, tokenObj.region);
-    speechConfig.speechRecognitionLanguage = speechRecognitionLanguage.current;
-    const audioConfig = speechsdk.AudioConfig.fromDefaultMicrophoneInput();
-    const recognizer = new speechsdk.SpeechRecognizer(speechConfig, audioConfig);
-    setDisplayText("speak into your microphone...");
-    recognizer.recognizeOnceAsync((result) => {
-      if (result.reason === speechsdk.ResultReason.RecognizedSpeech) {
-        setDisplayText(`Usuario: ${result.text}`);
-        //chatInputRef.current.value = result.text; // Actualiza el input del chat con el texto reconocido
-        talk(result.text);
-      } else {
-        setDisplayText("ERROR: Speech was cancelled or could not be recognized. Ensure your microphone is working properly.");
-      }
-    });
-  };
-
   const DID_API = {
     key: "bGljZW5zaW5nQGNldS5lcw:c_zuNVYnDitDPkm7uCmDZ",
     url: "https://api.d-id.com",
   };
 
 
+  //variable modo video que solo puede ser VIDDEO o AUDIO
+ // const modoVideoAudioChat = useRef('VIDEO');
 
-  const onIceGatheringStateChange = () => {
-    // Implementar la lógica aquí
-  };
 
-  function onIceCandidate(event) {
-    if (event.candidate) {
-      const { candidate, sdpMid, sdpMLineIndex } = event.candidate;
-  
-      fetch(`https://api.d-id.com/talks/streams/${streamIdRef.current}/ice`, {
-        method: 'POST',
-        headers: {
-          'Authorization': `Basic ${DID_API.key}`,
-          'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({
-          candidate,
-          sdpMid,
-          sdpMLineIndex,
-          session_id: sessionIdRef.current,
-        }),
-      });
-    }
-  };
 
  
-  const onIceConnectionStateChange = (event) => {
 
-    if (peerConnectionRef.current[indexConnection].connection.iceConnectionState === 'failed' || peerConnectionRef.current[indexConnection].connection.iceConnectionState === 'closed') {
-      stopAllStreams();
-      closePC();
-    }
-  }
-
-  const onConnectionStateChange = (event) => {
-    if (!peerConnectionRef.current[indexConnection].connection) return;
-    console.log(`Connection State Change: ${peerConnectionRef.current[indexConnection].connection.connectionState}`);
-  };
-
-  const onSignalingStateChange = (event) => {
-    if (!peerConnectionRef.current[indexConnection].connection) return;
-    console.log(`Signaling State Change: ${peerConnectionRef.current[indexConnection].connection.signalingState}`);
-  };
-
-  const onTrack = (event) => {
-    if (!peerConnectionRef.current[indexConnection].connection) return;
-    try {
-      if (talkVideoRef.current.srcObject) return;
-      talkVideoRef.current.srcObject = event.streams[0];
-    }
-    catch (e) {
-      console.error("Error during onTrack:", e);
-    }
-  };
-
+const MiComponente = () => {
+  const avatarImgUrl = useRef(IMG_URL_ROIG);
+  const azureVoiceId = useRef('es-ES-DarioNeural');
+  const avatar = useRef('roig');
+  const speechRecognitionLanguage = useRef('es-ES');
+  //const talkVideoRef = useRef(null);
+  //const  indexConnection = useRef(0);
+  
   const stopAllStreams = async() => {
-   try {
     if (talkVideoRef.current.srcObject) {
       console.log('stopping video streams');
       talkVideoRef.current.srcObject.getTracks().forEach(track => track.stop());
       talkVideoRef.current.srcObject = null;
     }
-    }
-    catch (e) {
-      console.error("Error during stopAllStreams:", e);
-    }
-  
-
   };
 
   const closePC =  () => {
@@ -236,7 +135,7 @@ const App = () => {
   };
   
 
-  const connect = async (indexConnection) => {
+  const  connect = async (indexConnection) => {
     console.log("Connect function invoked");
     if (peerConnectionRef.current[indexConnection].connection && peerConnectionRef.current[indexConnection].connection.connectionState === 'connected') {
       console.log("Already connected, exiting function");
@@ -344,60 +243,182 @@ const App = () => {
 
 
 
-  const configura_avatar = async (avatar_selecionado) => {
+  
 
-    if (peerConnectionRef.current[indexConnection].connection) { await closePC();}
-    avatar.current = avatar_selecionado;
-    console.log("Avatar:", avatar);
-    if (avatar.current === "camarero") {
-      console.log("Avatar: camarero");
-      //código para cambiar la imagen del avatar
-      avatarImgUrl.current= ( 'https://clips-presenters.d-id.com/rian/image.png');
-      azureVoiceId.current=('es-ES-DarioNeural')
-      speechRecognitionLanguage.current=('es-ES');
-      //talkVideoRef.current.poster = avatarImgUrl.current;
-      indexConnection = 0;
-    }
-    if (avatar.current === "profesor_ingles") {
-      console.log("Avatar: profesor inglés");
-      //código para cambiar la imagen del avatar
-      avatarImgUrl.current=( 'https://create-images-results.d-id.com/DefaultPresenters/Emily_f/image.jpeg');
-      azureVoiceId.current=( 'en-US-JennyNeural');
-      speechRecognitionLanguage.current=( "en-US");
-      //talkVideoRef.current.poster = avatarImgUrl.current;
-      indexConnection = 1;
-    }
-    if (avatar.current === "entrevistador") {
-      console.log("Avatar: entrevistador");
-      //código para cambiar la imagen del avatar
-      avatarImgUrl.current=( 'https://create-images-results.d-id.com/DefaultPresenters/Fatha_f/image.png');
-      azureVoiceId.current=('es-ES-LiaNeural' );
-      speechRecognitionLanguage.current =('es-ES');
-      //talkVideoRef.current.poster = avatarImgUrl.current;
-      indexConnection = 2;
-    }
-    if (avatar.current === "roig") {
-      console.log("Avatar: roig");
-      //código para cambiar la imagen del avatar
-      avatarImgUrl.current=(IMG_URL_ROIG);
-      azureVoiceId.current=('es-ES-DarioNeural');
-      speechRecognitionLanguage.current =('es-ES');
-      //talkVideoRef.current.poster = avatarImgUrl.current;
-      indexConnection = 3;
-    }
-    //talkVideoRef.current.poster = avatarImgUrl.current;
-    userRefIndexConnection.current = indexConnection;
-    await connect(indexConnection);
-    console.log("Configura avatar function completed successfully");
-  }
-  useEffect(() => {
-    if (talkVideoRef.current) {
-      talkVideoRef.current.poster = avatarImgUrl.current;
-    }
-  }, []); // Las dependencias están vacías, por lo que esto se ejecutará sólo una vez después del montaje del componente
+
   
+  const onIceGatheringStateChange = () => {
+    // Implementar la lógica aquí
+  };
+
+  function onIceCandidate(event) {
+    if (event.candidate) {
+      const { candidate, sdpMid, sdpMLineIndex } = event.candidate;
+  
+      fetch(`https://api.d-id.com/talks/streams/${streamIdRef.current}/ice`, {
+        method: 'POST',
+        headers: {
+          'Authorization': `Basic ${DID_API.key}`,
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+          candidate,
+          sdpMid,
+          sdpMLineIndex,
+          session_id: sessionIdRef.current,
+        }),
+      });
+    }
+  };
+
  
-  
+  const onIceConnectionStateChange = (event) => {
+
+    if (peerConnectionRef.current[indexConnection].connection.iceConnectionState === 'failed' || peerConnectionRef.current[indexConnection].connection.iceConnectionState === 'closed') {
+      stopAllStreams();
+      closePC();
+    }
+  }
+
+  const onConnectionStateChange = (event) => {
+    if (!peerConnectionRef.current[indexConnection].connection) return;
+    console.log(`Connection State Change: ${peerConnectionRef.current[indexConnection].connection.connectionState}`);
+  };
+
+  const onSignalingStateChange = (event) => {
+    if (!peerConnectionRef.current[indexConnection].connection) return;
+    console.log(`Signaling State Change: ${peerConnectionRef.current[indexConnection].connection.signalingState}`);
+  };
+
+  const onTrack = (event) => {
+    if (!peerConnectionRef.current[indexConnection].connection) return;
+    if (talkVideoRef.current.srcObject) return;
+    talkVideoRef.current.srcObject = event.streams[0];
+  };
+  // Definición de la función configura_avatar usando useCallback
+  const getTokenOrRefresh = async () => {
+    const response = await fetch(`https://${SPEECH_REGION}.api.cognitive.microsoft.com/sts/v1.0/issueToken`, {
+      method: "POST",
+      headers: {
+        "Ocp-Apim-Subscription-Key": SPEECH_KEY,
+      },
+    });
+
+    const authToken = await response.text();
+    return { authToken, region: SPEECH_REGION };
+  };
+
+  useEffect(() => {
+    const initialize = async () => {
+      const tokenRes = await getTokenOrRefresh();
+      if (tokenRes.authToken === null) {
+        setDisplayText(`FATAL_ERROR: ${tokenRes.error}`);
+      }
+    };
+    initialize();
+
+  }, []);
+
+  const sttFromMic = async () => {
+    const tokenObj = await getTokenOrRefresh();
+    const speechConfig = speechsdk.SpeechConfig.fromAuthorizationToken(tokenObj.authToken, tokenObj.region);
+    speechConfig.speechRecognitionLanguage = speechRecognitionLanguage.current;
+    const audioConfig = speechsdk.AudioConfig.fromDefaultMicrophoneInput();
+    const recognizer = new speechsdk.SpeechRecognizer(speechConfig, audioConfig);
+    setDisplayText("speak into your microphone...");
+    recognizer.recognizeOnceAsync((result) => {
+      if (result.reason === speechsdk.ResultReason.RecognizedSpeech) {
+        setDisplayText(`Usuario: ${result.text}`);
+        //chatInputRef.current.value = result.text; // Actualiza el input del chat con el texto reconocido
+        talk(result.text);
+      } else {
+        setDisplayText("ERROR: Speech was cancelled or could not be recognized. Ensure your microphone is working properly.");
+      }
+    });
+  };
+
+
+  const configura_avatar = useCallback(async (avatar_selecionado) => {
+    // Tu código aquí (el que has compartido)
+    // ...
+    
+
+      if (peerConnectionRef.current[0].connection) { await closePC();}
+      avatar.current = avatar_selecionado;
+      console.log("Avatar:", avatar);
+      if (avatar.current === "camarero") {
+        console.log("Avatar: camarero");
+        //código para cambiar la imagen del avatar
+        avatarImgUrl.current= ( 'https://clips-presenters.d-id.com/rian/image.png');
+        azureVoiceId.current=('es-ES-DarioNeural')
+        speechRecognitionLanguage.current=('es-ES');
+        talkVideoRef.current.poster = avatarImgUrl.current;
+        //indexConnection = 0;
+      }
+      if (avatar.current === "profesor_ingles") {
+        console.log("Avatar: profesor inglés");
+        //código para cambiar la imagen del avatar
+        avatarImgUrl.current=( 'https://create-images-results.d-id.com/DefaultPresenters/Emily_f/image.jpeg');
+        azureVoiceId.current=( 'en-US-JennyNeural');
+        speechRecognitionLanguage.current=( "en-US");
+        talkVideoRef.current.poster = avatarImgUrl.current;
+        //indexConnection = 1;
+      }
+      if (avatar.current === "entrevistador") {
+        console.log("Avatar: entrevistador");
+        //código para cambiar la imagen del avatar
+        avatarImgUrl.current=( 'https://clips-presenters.d-id.com/lily/ldwi8a_LdG/Zz10FZua2P/image.png');
+        azureVoiceId.current=('es-ES-LiaNeural' );
+        speechRecognitionLanguage.current =('es-ES');
+        talkVideoRef.current.poster = avatarImgUrl.current;
+        //indexConnection = 2;
+      }
+      if (avatar.current === "roig") {
+        console.log("Avatar: roig");
+        //código para cambiar la imagen del avatar
+        avatarImgUrl.current=(IMG_URL_ROIG);
+        azureVoiceId.current=('es-ES-DarioNeural');
+        speechRecognitionLanguage.current =('es-ES');
+        talkVideoRef.current.poster = avatarImgUrl.current;
+        //indexConnection = 3;
+      }
+      talkVideoRef.current.poster = avatarImgUrl.current;
+      userRefIndexConnection.current = indexConnection;
+      await connect(indexConnection);
+      console.log("Configura avatar function completed successfully");
+    
+  }, [closePC,connect]);  // Añade cualquier dependencia aquí, si es necesario
+
+  useEffect(() => {
+    // Código que utiliza configura_avatar
+    // ...
+    const urlParams = new URLSearchParams(window.location.search);
+    const avatarType = urlParams.get('avatar');
+    configura_avatar(avatarType)
+  }, [configura_avatar]);
+
+  return (
+    <main>
+    <video ref={talkVideoRef} autoPlay playsInline poster={avatarImgUrl.current} />
+    <p>{displayText}</p>
+    <button onClick={sttFromMic}>Pulsa para hablar</button>
+  </main>
+  );
+};
+
+
+
+
+
+
+ 
+
+
+
+
+
+
+
   return (
    
     <div className="container">
@@ -407,19 +428,10 @@ const App = () => {
       <header className="header">
         
         <h2 className="text-center">Avatar Chat</h2>
-        <nav className="menu">
-          <button onClick={() => configura_avatar("camarero")}>Camarero</button>
-          <button onClick={() => configura_avatar("profesor_ingles")}>Profesor de Inglés</button>
-          <button onClick={() => configura_avatar("entrevistador")}>Entrevistador</button>
-          <button onClick={() => configura_avatar("roig")}>Asistente Joseluis</button>
-        </nav>
+      <MiComponente></MiComponente>
       </header>
       
-      <main>
-        <video ref={talkVideoRef} autoPlay playsInline poster={avatarImgUrl.current} />
-        <p>{displayText}</p>
-        <button onClick={sttFromMic}>Pulsa para hablar</button>
-      </main>
+
     </div>
   );
   }  
